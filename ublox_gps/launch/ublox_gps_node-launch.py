cece7bc0f9ebd14fc5c16ff22c78378fe8738841
Tao Tien <29749622+taotien@users.noreply.github.com>
2026-05-25 12:48:01 -0700

    actually point to the right config path

--- ublox_gps/launch/ublox_gps_node-launch.py
+++ ublox_gps/launch/ublox_gps_node-launch.py
@@ -41,20 +41,24 @@ import launch_ros.actions
 
 def generate_launch_description():
     config_directory = os.path.join(
-        ament_index_python.packages.get_package_share_directory('ublox_gps'),
-        'config')
-    params = os.path.join(config_directory, 'neo_m9n.yaml')
-    ublox_gps_node = launch_ros.actions.Node(package='ublox_gps',
-                                             executable='ublox_gps_node',
-                                             output='both',
-                                             parameters=[params])
+        ament_index_python.packages.get_package_share_directory("ublox_gps"), "config"
+    )
+    params = os.path.join(config_directory, "neo-m9n.yaml")
+    ublox_gps_node = launch_ros.actions.Node(
+        package="ublox_gps",
+        executable="ublox_gps_node",
+        output="both",
+        parameters=[params],
+    )
 
-    return launch.LaunchDescription([ublox_gps_node,
-
-                                     launch.actions.RegisterEventHandler(
-                                         event_handler=launch.event_handlers.OnProcessExit(
-                                             target_action=ublox_gps_node,
-                                             on_exit=[launch.actions.EmitEvent(
-                                                 event=launch.events.Shutdown())],
-                                         )),
-                                     ])
+    return launch.LaunchDescription(
+        [
+            ublox_gps_node,
+            launch.actions.RegisterEventHandler(
+                event_handler=launch.event_handlers.OnProcessExit(
+                    target_action=ublox_gps_node,
+                    on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+                )
+            ),
+        ]
+    )
